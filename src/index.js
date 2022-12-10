77dcc4a4d409863e2f4b77173f9826c49b8f44e6
vres271 <vres271@gmail.com>
2022-12-10 21:40:01 +0400

    some

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import { App } from './app/app';
 import './style.css';
 
-document.body.innerHTML = `
-<h1>Hello Webpack!</h1>
-<p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, numquam cum minus amet molestias, expedita ea rem doloribus iste exercitationem voluptatum recusandae libero? Vero quisquam molestias temporibus quae sunt quis. 
-</p>    
-`;
-
 document.body.appendChild(App());
 
